d8c47c433d7d1b43334a6d114152ce7c6792b39d
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2020-11-03 12:41:30 +0800

    Initial Commit for Assignment 3

---
+++ Assignment 3/electricity_bill_batch_processor/electricity_bill_batch_processor/Main.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Jason Ang Chia Wuen
+Student ID: 100087252
+Date: 
+Program Description: 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Yeet!" << endl;
+
+	return 0;
+}
---
+++ Assignment 3/electricity_bill_batch_processor/electricity_bill_batch_processor/tariff.cpp
@@ -0,0 +1,70 @@
+//#include "tariff.h"
+
+/**
+ * Calculate monthly electricity charge at domestic rate based on units consumed
+ *
+ * @param unitsConsumed - the number of units in kwh consumed in current month
+ * @return charge - calculated charge, minimum 5.00
+ */
+double getMonthlyChargeDomestic(double unitsConsumed)
+{
+	double charge;
+	if (unitsConsumed > 1300)
+		charge = unitsConsumed * 0.315;
+	else if (unitsConsumed > 800)
+		charge = unitsConsumed * 0.31;
+	else if (unitsConsumed > 700)
+		charge = unitsConsumed * 0.305;
+	else if (unitsConsumed > 500)
+		charge = unitsConsumed * 0.3;
+	else if (unitsConsumed > 400)
+		charge = unitsConsumed * 0.295;
+	else if (unitsConsumed > 300)
+		charge = unitsConsumed * 0.27;
+	else if (unitsConsumed > 200)
+		charge = unitsConsumed * 0.25;
+	else if (unitsConsumed > 150)
+		charge = unitsConsumed * 0.22;
+	else 
+		charge = unitsConsumed * 0.18;
+	
+	if (charge < 5.00)
+		charge = 5.00;
+
+	return charge;
+}
+
+
+/**
+ * Calculate monthly electricity charge at commercial rate based on units consumed
+ *
+ * @param unitsConsumed - the number of units in kwh consumed in current month
+ * @return charge - calculated charge, minimum 10.00
+ */
+double getMonthlyChargeCommercial(double unitsConsumed)
+{
+	double charge;
+	if (unitsConsumed > 20000)
+		charge = unitsConsumed * 0.30;
+	else if (unitsConsumed > 10000)
+		charge = unitsConsumed * 0.31;
+	else if (unitsConsumed > 3000)
+		charge = unitsConsumed * 0.32;
+	else if (unitsConsumed > 500)
+		charge = unitsConsumed * 0.315;
+	else if (unitsConsumed > 400)
+		charge = unitsConsumed * 0.3;
+	else if (unitsConsumed > 300)
+		charge = unitsConsumed * 0.28;
+	else if (unitsConsumed > 200)
+		charge = unitsConsumed * 0.26;
+	else if (unitsConsumed > 100)
+		charge = unitsConsumed * 0.24;
+	else
+		charge = unitsConsumed * 0.20;
+
+	if (charge < 10.00)
+		charge = 10.00;
+
+	return charge;
+}
